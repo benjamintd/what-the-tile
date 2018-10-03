8650b4ed465132f0b332a5025faab94f635fd2b2
Benjamin Tran Dinh <benjamin@mapbox.com>
2018-10-03 13:20:14 +0200

    it's a start

---
+++ index.js
@@ -0,0 +1,183 @@
+const MapboxGeocoder = require('@mapbox/mapbox-gl-geocoder');
+const mapboxgl = require('mapbox-gl');
+const tilebelt = require('@mapbox/tilebelt');
+const tc = require('@mapbox/tile-cover');
+
+mapboxgl.accessToken = 'pk.eyJ1IjoiYmVuamFtaW50ZCIsImEiOiJjaW83enIwNjYwMnB1dmlsejN6cDBzbm93In0.0ZOGwSLp8OjW6vCaEKYFng';
+
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/benjamintd/cjmt1av8w1dto2so7ijtr4b67',
+  center: [0, 25],
+  zoom: 1.3
+});
+
+var geocoder = new MapboxGeocoder({
+  accessToken: mapboxgl.accessToken
+})
+
+map.addControl(geocoder);
+
+
+
+map.on('load', () => {
+  map.addSource('tiles-geojson', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
+
+  map.addSource('tiles-centers-geojson', {
+    type: 'geojson',
+    data: {
+      type: 'FeatureCollection',
+      features: []
+    }
+  });
+
+  map.addLayer({
+    id: 'tiles',
+    source: 'tiles-geojson',
+    type: 'line',
+    paint: {
+      'line-color': '#000'
+    }
+  });
+
+  map.addLayer({
+    id: 'tiles-shade',
+    source: 'tiles-geojson',
+    type: 'fill',
+    paint: {
+      'fill-color': ['case', ['get', 'even'], 'rgba(0,0,0,0.1)', 'rgba(0,0,0,0)']
+    }
+  });
+
+  map.addLayer({
+    id: 'tiles-centers',
+    source: 'tiles-centers-geojson',
+    type: 'symbol',
+    layout: {
+      'text-field': ['format', ['get', 'text'], { 'font-scale': 1.2 }],
+      'text-offset': [0, -1],
+    },
+    paint: {
+      'text-color': '#000',
+      'text-color-transition': {
+        duration: 0
+      },
+      'text-halo-color': '#fff',
+      'text-halo-width': 0.5
+    }
+  });
+
+  update();
+});
+
+map.on('moveend', update);
+
+map.on('click', (e) => {
+  features = map.queryRenderedFeatures(e.point, {layers: ['tiles-shade']});
+  console.log(features)
+  copyToClipboard(features[0].properties.quadkey)
+})
+
+function updateGeocoderProximity() {
+  // proximity is designed for local scale, if the user is looking at the whole world,
+  // it doesn't make sense to factor in the arbitrary centre of the map
+  if (map.getZoom() > 9) {
+    var center = map.getCenter().wrap(); // ensures the longitude falls within -180 to 180 as the Geocoding API doesn't accept values outside this range
+    geocoder.setProximity({ longitude: center.lng, latitude: center.lat });
+  } else {
+    geocoder.setProximity(null);
+  }
+}
+
+function update() {
+  updateGeocoderProximity();
+  updateTiles();
+}
+
+function updateTiles() {
+  var extentsGeom = getExtentsGeom();
+  var zoom = Math.ceil(map.getZoom());
+  tiles = tc.tiles(extentsGeom, {min_zoom: zoom, max_zoom: zoom});
+
+  map.getSource('tiles-geojson').setData({
+    type: 'FeatureCollection',
+    features: tiles.map(getTileFeature)
+  });
+
+  map.getSource('tiles-centers-geojson').setData({
+    type: 'FeatureCollection',
+    features: tiles.map(getTileCenterFeature)
+  });
+}
+
+function getExtentsGeom() {
+  var e = map.getBounds();
+  var box = [
+    e.getSouthWest().toArray(),
+    e.getNorthWest().toArray(),
+    e.getNorthEast().toArray(),
+    e.getSouthEast().toArray(),
+    e.getSouthWest().toArray()
+  ].map(coords => {
+    if (coords[0] < -180) return [-179.99999, coords[1]]
+    if (coords[0] > 180) return [179.99999, coords[1]]
+    return coords
+  });
+
+  return {
+    type: 'Polygon',
+    coordinates: [box]
+  };
+}
+
+function getTileFeature(tile) {
+  var quadkey = tilebelt.tileToQuadkey(tile);
+
+  var feature = {
+    type: 'Feature',
+    properties: {
+      even: ((tile[0] + tile[1]) % 2 == 0),
+      quadkey: quadkey
+    },
+    geometry: tilebelt.tileToGeoJSON(tile)
+  };
+  return feature;
+}
+
+function getTileCenterFeature(tile) {
+  var box = tilebelt.tileToBBOX(tile);
+  var center = [
+    (box[0] + box[2]) / 2,
+    (box[1] + box[3]) / 2
+  ];
+
+  var quadkey = tilebelt.tileToQuadkey(tile);
+
+  return {
+    type: 'Feature',
+    properties: {
+      text: 'Tile: ' + JSON.stringify(tile) + '\nQuadkey: ' + quadkey + '\nZoom: ' + tile[2],
+      quadkey: quadkey
+    },
+    geometry: {
+      type: 'Point',
+      coordinates: center
+    }
+  };
+}
+
+
+function copyToClipboard (str) {
+  const el = document.createElement('textarea');
+  el.value = str;
+  document.body.appendChild(el);
+  el.select();
+  document.execCommand('copy');
+  document.body.removeChild(el);
+};
